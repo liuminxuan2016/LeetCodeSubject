cada4aa9f9d3861837e694620d91a7b528895bab
刘敏轩 <liuminxuan2016@gmail.com>
2020-05-22 08:23:24 +0800

    0520

--- LeetCode-Cpp/1281. 整数的各位积和之差/solution_2.cpp
+++ LeetCode-Cpp/1281. 整数的各位积和之差/solution_2.cpp
@@ -7,7 +7,16 @@ using namespace std;
 class Solution {
 public:
 	int subtractProductAndSum(int n) {
-
+		int pro = 1;
+		int sum = 0;
+		while (n)
+		{
+			int m = n % 10;
+			n /= 10;
+			pro *= m;
+			sum += m;
+		}
+		return pro - sum;
 	}
 };
 
